429df88eff34328196f3b3446b3757f45ff870c5
kanav20 <kanavmalik20@gmail.com>
2018-07-10 00:37:41 +0530

    connected to mongoDB atlas, adding models is left

--- app.js
+++ app.js
@@ -1,16 +1,36 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan'); 
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
 
+mongoose.connect(
+    'mongodb://nodeShop:'
+     +process.env.MONGO_ATLAS_PW+
+     '@node-rest-shop-shard-00-00-cywta.mongodb.net:27017,node-rest-shop-shard-00-01-cywta.mongodb.net:27017,node-rest-shop-shard-00-02-cywta.mongodb.net:27017/test?ssl=true&replicaSet=node-rest-shop-shard-0&authSource=admin&retryWrites=true',
+     {
+         useMongoClient : true,
+     }
+    );
 app.use(morgan('dev'));
-// app.use((req,res, next)=>{
-//     res.status(200).json({
-//         message: "it works",
-//     });
-// });
+app.use(bodyParser.urlencoded({extended : false})); //this is the middleWare
+app.use(bodyParser.json());
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
+    if (req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+        return res.status(200).json({});
+    }
+    next();
+  });
+
 app.use('/products', productRoutes);
 app.use('/orders',orderRoutes);
 
